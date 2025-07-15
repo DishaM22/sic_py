975446599789dc7010dfe4c95c80e46346e71aed
DishaM22 <dishamanjunath22@gmail.com>
2025-07-15 11:58:18 +0530

    postfix

--- week 3/infix_to_postfix.py
+++ week 3/infix_to_postfix.py
@@ -1,43 +1,42 @@
 def precedence(op):
-    if op=='+' or op=='-' :
+    if op in ('+', '-'):
         return 1
-    if op=='*' or op=='/':
+    elif op in ('*', '/'):
         return 2
-    if op=='^' or op=='% ':
+    elif op in ('^', '%'):
         return 3
     return 0
+
 def right_associativity(op):
-    return True
-def  infix_to_postfix(expr):
-    values=[]
-    stack=[]
-    i=0
+    return op == '^'
+
+def infix_to_postfix(expr):
+    values = []
+    stack = []
+    expr = expr.replace(" ", "")  # Clean expression
 
     for i in expr:
-        if i.isdigit():
+        if i.isalnum():  # Operand: letter or digit
             values.append(i)
-        elif i=="(":
+        elif i == '(':
             stack.append(i)
-        elif i==")":
-            while stack and stack[-1]!="(" :
+        elif i == ')':
+            while stack and stack[-1] != '(':
                 values.append(stack.pop())
-            stack.pop()
-        else:
-            while (stack and stack[-1]!="(" and (precedence(stack[-1])>precedence(i)) or (precedence(stack[-1]))==precedence(i) and not right_associative(i)):
+            stack.pop()  # remove '('
+        else:  # Operator
+            while (stack and stack[-1] != '(' and
+                   (precedence(stack[-1]) > precedence(i) or
+                    (precedence(stack[-1]) == precedence(i) and not right_associativity(i)))):
                 values.append(stack.pop())
             stack.append(i)
+
     while stack:
         values.append(stack.pop())
-        return ''.join(output)
 
-expr="4*2*(3^2*5-4)%2"
-print("postfix expr:" ,infix_to_postfix(expr))
+    return ' '.join(values)
 
-
-        
-
-
-   
-
-
-            
+# Run this exactly
+expr = "a * (b + c / d) ^ e - f"
+postfix = infix_to_postfix(expr)
+print("Postfix expr:", postfix)
